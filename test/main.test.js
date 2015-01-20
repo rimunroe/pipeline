961751b9fac8d09f828b885f6e460fb65f0f3261
Richard Munroe <rimunroe@gmail.com>
2015-01-20 18:54:19 -0500

    reskeletoned tests

--- test/main.test.js
+++ test/main.test.js
@@ -1,176 +1,113 @@
 var chai = require('chai');
+var pipeline = require('../dist/pipeline.js');
 var should = chai.should();
 
-describe('Pipeline', function(){
-  var pipeline;
+describe('While defining an app', function(){
 
-  before(function(){
-    pipeline = require('../dist/pipeline.js');
+  describe('creating a single store', function(){
+
+    it('throws an error when creating a store which depends on itself', function(){
+      // TODO
+    });
+
+    it('throws an error when using reserved keys in the api object', function(){
+      // TODO
+    });
+
   });
 
-  it('should expose a method to create a new app', function(){
-    pipeline.createApp.should.exist();
+  describe('creating multiple stores', function(){
+
+    it('throws an error when creating two mutually dependent stores', function(){
+      // TODO
+    });
+
+    it('throws an error when creating three stores with a circular dependency', function(){
+      // TODO
+    });
+
+    describe('the ways to define dependencies', function(){
+      // TODO
+    });
+
   });
 
-  describe('Creating apps', function(){
-    it('Should return a new app', function(){
-      var App = pipeline.createApp();
-      App.should.be.an('object');
-    });
-
-    it('should allow you to create multiple pipeline apps', function(){
-      var App1 = pipeline.createApp();
-      var App2 = pipeline.createApp();
-
-      App1.should.not.equal(App2);
-    });
-  });
-
-  describe('Creating stores', function(){
-    it('should throw an error if you create a circular dependency', function(){
-      var App = pipeline.createApp();
-
-      App.createStore('foo', {
-        after: 'bar',
-        actions:{
-          boop: function(){}
-        }
-      });
-
-      var creatingBadStore = function(){
-        App.createStore('bar', {
-          after: 'foo',
-          actions: {
-            boop: function(){}
-          }
-        });
-      };
-
-      creatingBadStore.should.throw(Error);
-    });
-  });
-
-  describe('A simple pipeline app', function(){
-    var App, output;
-
-    beforeEach(function(){
-      output = undefined;
-      App = pipeline.createApp();
-      App.createAction('newNumber', function(number){
-        return {data: number};
-      });
-      App.createStore('numberStore', {
-        initialize: function(){
-          this.update({number: 0});
-        },
-        actions: {
-          newNumber: function(payload){
-            this.update({number: payload.data});
-          }
-        }
-      });
-      App.createAdapter('someAdapter', {
-        initialize: function(){
-          output = this.stores.numberStore.get('number');
-        },
-        stores: {
-          numberStore: function(){
-            output = this.stores.numberStore.get('number');
-          }
-        }
-      });
-
-      App.start();
-    });
-
-    afterEach(function(){
-      output = undefined;
-    });
-
-    it('should have created the action', function(){
-      App.actions.newNumber.should.exist()
-    });
-
-    it('should have created the store', function(){
-      App.stores.numberStore.should.exist()
-    });
-
-    describe('the adapter', function(){
-      it('should have updated the tracking value to the correct value', function(){
-        output.should.equal(0)
-      });
-    });
-
-    describe('sending a single action', function(){
-      it('should result in the store updating itself and the adapter reacting to the change', function(){
-        App.actions.newNumber(1);
-        output.should.equal(1);
-      });
-    });
-
-    describe('sending multiple actions', function(){
-      it('should cause effects synchronously', function(){
-        App.actions.newNumber(2);
-        output.should.equal(2);
-        App.actions.newNumber(3);
-        output.should.equal(3);
-      });
-    });
-  });
-
-  describe('An app with two stores where one waits for the other', function(){
-    var App, output;
-
-    beforeEach(function(){
-      output = undefined;
-      App = pipeline.createApp();
-      App.createAction('newNumber', function(number){
-        return {data: number};
-      });
-      App.createStore('leader', {
-        initialize: function(){
-          this.update({number: 0});
-        },
-        actions: {
-          newNumber: function(payload){
-            this.update({number: payload.data});
-          }
-        }
-      });
-      App.createStore('follower', {
-        after: ['leader'],
-        initialize: function(){
-          this.update({number: 0});
-        },
-        actions: {
-          newNumber: function(payload){
-            this.update({number: this.stores.leader.get('number') * 2});
-          }
-        }
-      });
-      App.createAdapter('someAdapter', {
-        initialize: function(){
-          output = this.stores.follower.get('number');
-        },
-        stores: {
-          follower: function(){
-            output = this.stores.follower.get('number');
-          }
-        }
-      });
-
-      App.start();
-    });
-
-    afterEach(function(){
-      output = undefined;
-    });
-
-    describe('two stores with an evaluation order', function(){
-      it('should evaluate in the correct order', function(){
-        App.actions.newNumber(5);
-        output.should.equal(10);
-      });
-    });
-  });
 });
+
+describe('While starting an app', function(){
+
+  describe('the initialization functions', function(){
+
+    describe('of stores', function(){
+
+      it('is called first', function(){
+        // TODO
+      });
+
+    });
+
+    describe('of adapters', function(){
+
+      it('is called second', function(){
+        // TODO
+      });
+
+    });
+
+    describe('of the app', function(){
+
+      it('is called third', function(){
+        // TODO
+      });
+
+    });
+
+  });
+
+});
+
+describe('While an app is running', function(){
+
+  it('is not possible to create new actions', function(){
+    // TODO
+  });
+
+  it('is not possible to create new stores', function(){
+    // TODO
+  });
+
+  describe('sending multiple actions in quick succession', function(){
+
+    it('resolves synchronously', function(){
+      // TODO
+    });
+
+  });
+
+  describe('the reactions of stores to actions', function(){
+
+    it('happen in proper order', function(){
+      // TODO
+    });
+
+  });
+
+  describe('adapter reactions', function(){
+
+    it('occur in response to stores changing', function(){
+      // TODO
+    });
+
+  });
+
+  describe('creating and running another app at the same time', function(){
+
+    it('does not interfere with the app', function(){
+      // TODO
+    });
+
+  });
+
+});
+
+
