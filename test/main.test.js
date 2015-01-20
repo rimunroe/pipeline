df16539dec82b39036ca142589e6d9749c3618b9
Richard Munroe <rimunroe@gmail.com>
2015-01-20 14:35:16 -0500

    added a few tests

---
+++ test/main.test.js
@@ -0,0 +1,97 @@
+var chai = require('chai')
+var should = chai.should()
+// var pipeline = require('../src/main.js');
+
+describe('Pipeline', function(){
+  var pipeline;
+
+  before(function(){
+    pipeline = require('../dist/pipeline.js');
+  });
+
+  it('should expose a method to create a new app', function(){
+    pipeline.createApp.should.exist();
+  });
+
+  describe('Creating apps', function(){
+    it('Should return a new app', function(){
+      var App = pipeline.createApp();
+      App.should.be.an('object');
+    });
+
+    it('should allow you to create multiple pipeline apps', function(){
+      var App1 = pipeline.createApp();
+      var App2 = pipeline.createApp();
+
+      App1.should.not.equal(App2);
+    })
+  });
+
+  describe('A simple pipeline app', function(){
+    var App, output;
+
+    beforeEach(function(){
+      output = undefined;
+      App = pipeline.createApp();
+      App.createAction('newNumber', function(number){
+        return {data: number};
+      });
+      App.createStore('numberStore', {
+        initialize: function(){
+          this.update({number: 0});
+        },
+        actions: {
+          newNumber: function(payload){
+            this.update({number: payload.data});
+          }
+        }
+      });
+      App.createAdapter('someAdapter', {
+        initialize: function(){
+          output = this.stores.numberStore.get('number');
+        },
+        stores: {
+          numberStore: function(){
+            output = this.stores.numberStore.get('number');
+          }
+        }
+      });
+
+      App.start();
+    });
+
+    afterEach(function(){
+      output = undefined;
+    });
+
+    it('should have created the action', function(){
+      App.actions.newNumber.should.exist()
+    });
+
+    it('should have created the store', function(){
+      App.stores.numberStore.should.exist()
+    });
+
+    describe('the adapter', function(){
+      it('should have updated the tracking value to the correct value', function(){
+        output.should.equal(0)
+      });
+    });
+
+    describe('sending a single action', function(){
+      it('should result in the store updating itself and the adapter reacting to the change', function(){
+        App.actions.newNumber(1);
+        output.should.equal(1);
+      });
+    });
+
+    describe('sending multiple actions', function(){
+      it('should cause effects synchronously', function(){
+        App.actions.newNumber(2);
+        output.should.equal(2);
+        App.actions.newNumber(3);
+        output.should.equal(3);
+      });
+    });
+  });
+});
