3b18fd8a547d08ad7a3f4fd813815a4ffc3e64f7
William Lubelski <will.lubelski@gmail.com>
2015-02-02 23:46:33 -0500

    allow name-spacing of helpers

--- src/factories/makeCreateHelper.js
+++ src/factories/makeCreateHelper.js
@@ -3,6 +3,7 @@ pipeline._makeCreateHelper = function (_app) {
     if (_app.hasStarted) {
       throw new Error("cannot create new helper \"" + helperName + "\". App has already started.");
     }
+
     var _context = {
       helpers: _app.helpers
     }
@@ -10,9 +11,22 @@ pipeline._makeCreateHelper = function (_app) {
     var helper = function (){
       fxn.apply(_context, arguments);
     }
+    helper.helperName = helperName;
 
-    _app.helpers[helperName] = helper;
+    var keys = helperName.split('.');
+    var slot = _app.helpers;
+    var namespaces = keys.slice(0, -1)
+    var lastKey = keys.slice(-1)[0]
+    _.each(namespaces, function (key) {
+      if (!slot[key]) {
+        slot[key] = {};
+      }
+      slot = slot[key]
+    });
+
+    slot[lastKey] = helper;
 
     return helper;
+
   };
 };
