caaf7431687c83bdae8855006a0e603e25c68243
William Lubelski <will.lubelski@gmail.com>
2015-02-02 22:57:50 -0500

    hang actions off of _app

--- src/factories/makeCreateAction.js
+++ src/factories/makeCreateAction.js
@@ -4,9 +4,21 @@ pipeline._makeCreateAction = function (_app) {
       throw new Error("cannot create new action \"" + actionName + "\". App has already started.");
     }
 
-    _app.actions[actionName] = function (){
-      var payload = packager.apply(null, arguments);
-      _app.dispatcher.enqueueAction(actionName, typeof payload === 'object' ? payload : {});
+    var action = function (){
+      if (!_app.hasStarted) {
+        throw new Error("Action \"" + actionName + "\" attempted to fire before the app was started.");
+      } else {
+        var payload = packager.apply(null, arguments);
+        _app.dispatcher.enqueueAction(actionName, typeof payload === 'object' ? payload : {});
+      }
+      return payload ? true : false
     };
+
+    action.actionName = actionName
+
+    _app.actions[actionName] = action;
+
+    return action
+
   };
 };
