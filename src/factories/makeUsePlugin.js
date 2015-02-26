bd6c262cc5d3224cfc9f8390b94576efe2655c4c
Richard Munroe <rimunroe@gmail.com>
2015-02-26 14:10:04 -0500

    changed usePlugin to take two arguments

--- src/factories/makeUsePlugin.js
+++ src/factories/makeUsePlugin.js
@@ -1,27 +1,25 @@
 var _makeUsePlugin = function (_app) {
-  return function (options) {
-    if (options.name == null) throw new Error("Plugin must be named");
+  return function (name, options) {
+    if (name == null) throw new Error("Plugin must be named");
 
-    var pluginName = options.name;
+    if (options != null) {
 
-    if (options.interface != null) {
-
-      if (typeof options.interface.factories === 'object') {
-        var badKeys = _.intersection(_.keys(options.interface.factories, _app.create));
+      if (typeof options.factories === 'object') {
+        var badKeys = _.intersection(_.keys(options.factories, _app.create));
         if (!_.isEmpty(badKeys)) {
           throw new Error("Plugin \"" + pluginName + "\" attempts to overwrite the following keys on app.create: " + badKeys.join(', '));
         }
         var _creators = {};
 
-        _.forEach(options.interface.factories, function(factory, creatorName){
+        _.forEach(options.factories, function(factory, creatorName){
           _.creators[creatorName] = factory.call(null, _app);
         });
 
         _.extend(_app.create, _creators);
       }
 
-      if (typeof options.interface.startHook === 'function') {
-        _app.startHooks[pluginName] = options.interface.startHook;
+      if (typeof options.startHook === 'function') {
+        _app.startHooks[pluginName] = options.startHook;
       }
     }
   };
